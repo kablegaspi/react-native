e6156799daa18bfd8dec934f773f80d8c11114f7
Krizthel Legaspi <kablegaspi@gmail.com>
2020-05-05 21:50:49 -0400

    animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     
     render() {
         return (
             <ScrollView>
-                <Card
-                title='Contact Information'
-                wrapperStyle={{margin: 10}}>
-                <Text style={{marginBottom: 10}}>
-                    1 Nucamp Way {"\n"}
-                        Seattle, WA 98001 {"\n"}
-                        U.S.A.{"\n"}{"\n"}
-                        Phone: 1-206-555-1234{"\n"}
-                        Email: campsites@nucamp.co
-                </Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                        title='Contact Information'
+                        wrapperStyle={{margin: 10}}>
+                        <Text style={{marginBottom: 10}}>
+                            1 Nucamp Way {"\n"}
+                                Seattle, WA 98001 {"\n"}
+                                U.S.A.{"\n"}{"\n"}
+                                Phone: 1-206-555-1234{"\n"}
+                                Email: campsites@nucamp.co
+                        </Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
